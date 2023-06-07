a6c8ee2e36f4f6dad78d5ef7754f167a272398ac
Poonampancheshwar <Poonampancheshwar1999@gmail.com>
2023-06-07 22:29:03 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { type } from 'os';
+const reducerFun = (state:any,action:any)=> {
+  console.log('state', state);
+  console.log('action', action);
+  switch (action.type) {
+    case "increment":
+      const newState ={
+        ...state,
+        counter:state.counter +1,
+        status:action.type
+      }
+      return newState
+    case "decrement":
+      const newState2 ={
+        ...state,
+        counter:state.counter - 1,
+        status:action.type
+      }
+      return newState2
+    default:
+      return state
+     
+  }
+};
+const initialState = {counter:0, state: ''}
 function App() {
+  const [state, dispatch] = useReducer(reducerFun, initialState);
+  const incrementValue =()=> {
+    console.log('increment', state);
+  console.log("incrementValue");
+  dispatch({ type:'increment' });
+  }
+   const decrementValue =()=> {
+    console.log('decrement', state);
+    console.log("decrementValue");
+    dispatch({type:'decrement'});
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br />
+       Counter = {state.counter} 
+      <br />
+       status = {state.status} 
+      <br />
+      <button type="button" onClick={incrementValue}>increment</button>
+      <button type ="button" onClick={decrementValue}>decrement</button>
     </div>
   );
 }
